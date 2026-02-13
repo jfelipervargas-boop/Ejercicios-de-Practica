dddba5059dc02d3a27bda67118cc47436a5ccdf4
Felipe_Rodriguez <jfelipervargas@gmail.com>
2026-02-13 13:52:53 -0500

    Update Calc_Interes_Compuesto.cpp

--- Calc_Interes_Compuesto.cpp
+++ Calc_Interes_Compuesto.cpp
@@ -5,21 +5,17 @@ using namespace std;
 int main() {
     double P, r, n, t, A;
 
-    cout << "P: ";
     cin >> P;
 
-    cout << "r: ";
     cin >> r;
 
-    cout << "n: ";
     cin >> n;
 
-    cout << "t: ";
     cin >> t;
 
     A = P * pow((1 + r / n), n * t);
 
-    cout << "A: " << A << endl;
+    cout << A << endl;
 
     return 0;
 }
